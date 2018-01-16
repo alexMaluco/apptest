1d5ec96b36c9c1b388b68fdae497613c53627b99
FERMAN <alex.barbosa.r@gmail.com>
2018-01-16 17:08:48 -0300

    Criação de form para salvar o novo-amigo no banco usando MongoDB

--- app/controllers/novo-amigo.js
+++ app/controllers/novo-amigo.js
@@ -1,4 +1,18 @@
 import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
+  mensagemDeResposta: '',
+  email: '',
+
+  isValid: match('email', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  actions: {
+    salva() {
+      alert(`Ta salvando a parada: ${this.get('email')}`);
+      this.set('mensagemDeResposta', `Valeu salvou aqui: ${this.get('email')}`);
+      this.set('email', '');
+    }
+  }
 });
