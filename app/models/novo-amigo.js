c7ea252c0af85be856e68e599079683bfa7ead7d
FERMAN <alex.barbosa.r@gmail.com>
2018-01-18 16:48:28 -0300

    Instalação do express e atualização no model de novo-amigo

--- app/models/novo-amigo.js
+++ app/models/novo-amigo.js
@@ -1,4 +1,11 @@
 import DS from 'ember-data';
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+
+});
 
 export default DS.Model.extend({
 
